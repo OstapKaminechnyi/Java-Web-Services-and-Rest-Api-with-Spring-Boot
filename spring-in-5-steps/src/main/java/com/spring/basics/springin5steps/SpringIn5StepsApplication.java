70d0e21c8ea3b06361e1d6eb5ab487ebe303a6a5
OstapKaminechnyi <ostap1599@gmail.com>
2019-01-22 22:31:58 +0200

    use spring to manage dependencies - @Component, @Autowired

--- spring-in-5-steps/src/main/java/com/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,15 +2,23 @@ package com.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
+    //beans
 
 	public static void main(String[] args) {
-        BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+       // BinarySearchImpl binarySearch =
+         //       new BinarySearchImpl(new BubbleSortAlgorithm());
+        //Application context
+
+
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
         int result = binarySearch.binarySearch(new int[] {12,4,6},3);
-        System.out.print(result);
-		//SpringApplication.run(SpringIn5StepsApplication.class, args);
+
+        System.out.println(result);
 	}
 
 }
