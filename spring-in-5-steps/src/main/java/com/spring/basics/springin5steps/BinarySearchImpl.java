b24edecf1d7d1c5352577c045fdfd9db36ba4253
OstapKaminechnyi <ostap1599@gmail.com>
2019-01-23 21:07:03 +0200

    constructor and setter injection

--- spring-in-5-steps/src/main/java/com/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,17 +1,16 @@
 package com.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BinarySearchImpl {
 
     @Autowired
     private SortAlgorithm sortAlgorithm;
 
-    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-        this.sortAlgorithm = sortAlgorithm;
-    }
 
-    public int binarySearch(int[] numbers, int numbersToSearchFor){
+    public int binarySearch(int[] numbers, int numbersToSearchFor) {
         int[] sortedNumbers = sortAlgorithm.sort(numbers);
         System.out.println(sortAlgorithm);
 
