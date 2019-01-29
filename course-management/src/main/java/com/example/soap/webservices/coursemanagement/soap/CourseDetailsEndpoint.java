d634885582a3ab1e95aa40d26aba54ccd2879bdd
OstapKaminechnyi <ostap1599@gmail.com>
2019-01-29 18:45:57 +0200

    implementing soap web services for getallcoursedetailsrequest

--- course-management/src/main/java/com/example/soap/webservices/coursemanagement/soap/CourseDetailsEndpoint.java
+++ course-management/src/main/java/com/example/soap/webservices/coursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,16 +1,16 @@
 package com.example.soap.webservices.coursemanagement.soap;
 
+import com.in28minutes.courses.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-import com.in28minutes.courses.CourseDetails;
-import com.in28minutes.courses.GetCourseDetailsRequest;
-import com.in28minutes.courses.GetCourseDetailsResponse;
 import com.example.soap.webservices.coursemanagement.soap.bean.Course;
 import com.example.soap.webservices.coursemanagement.soap.service.CourseDetailsService;
+
+import java.util.List;
 
 @Endpoint
 public class CourseDetailsEndpoint {
@@ -30,12 +30,25 @@ public class CourseDetailsEndpoint {
 
         Course course = service.findById(request.getId());
 
-        return mapCourse(course);
+        return mapCourseDetails(course);
     }
 
-    private GetCourseDetailsResponse mapCourse(Course course) {
+    private GetCourseDetailsResponse mapCourseDetails(Course course) {
         GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+        response.setCourseDetails(mapCourse(course));
+        return response;
+    }
 
+    private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+        GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+        for (Course course : courses) {
+            CourseDetails mapCourse = mapCourse(course);
+            response.getCourseDetails().add(mapCourse);
+        }
+        return response;
+    }
+
+    private CourseDetails mapCourse(Course course) {
         CourseDetails courseDetails = new CourseDetails();
 
         courseDetails.setId(course.getId());
@@ -43,10 +56,17 @@ public class CourseDetailsEndpoint {
         courseDetails.setName(course.getName());
 
         courseDetails.setDescription(course.getDescription());
+        return courseDetails;
+    }
 
-        response.setCourseDetails(courseDetails);
+    @PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetAllCourseDetailsRequest")
+    @ResponsePayload
+    public GetAllCourseDetailsResponse processAllCourseDetailsRequest(
+            @RequestPayload GetAllCourseDetailsRequest request) {
 
-        return response;
+        List<Course> courses = service.findAll();
+
+        return mapAllCourseDetails(courses);
     }
 
 }
